561cce0b712881141b50ad18c65e01da5126ac6a
shinomy <c6302012@st.kanazawa-it.ac.cjp>
2023-11-14 13:40:55 +0900

    add sign() function to util.hpp

--- roomba_basic_noGUI/include/util.hpp
+++ roomba_basic_noGUI/include/util.hpp
@@ -5,4 +5,7 @@ namespace util {
 inline static constexpr double pi_d = 3.1415926535897932384626433832795;
 inline static constexpr float pi_f = static_cast<float>(pi_d);
 
+template <class T>
+constexpr int sign(T a) { return a >= 0 ? 1 : -1;}
+
 }  // namespace util
