392c77b24200914bc38d5dd28edc1b261bbff815
shinomy <c6302012@st.kanazawa-it.ac.cjp>
2023-12-05 08:56:00 +0900

    add motion_captured_logfiles and data organizer.

---
+++ motion_captured_logfiles/organize_raw_data.m
@@ -0,0 +1,14 @@
+path = "raw_data/Take 2023-11-28 11.09.52 AM.csv"
+[filepath,name,ext] = fileparts(path)
+splited_name = strsplit(name)
+
+raw_csv = readmatrix(path);
+time = raw_csv(6:end, 2);
+x = raw_csv(6:end, 6);
+y = -1*raw_csv(6:end, 8);
+theta = deg2rad(raw_csv(6:end,4));
+
+output_dir = "organized_data\";
+output_name = splited_name(2)+ "_" + splited_name(3) + ext;
+writematrix([time, x, y, theta], output_dir + output_name)
+
---
+++ motion_captured_logfiles/organized_data/csv_plotter.m
@@ -0,0 +1,52 @@
+clear;
+csv = readmatrix("2023-11-28_11.09.52.csv");
+time = csv(:,1) - csv(1,1);
+x = csv(:,2);
+y = csv(:,3);
+theta = csv(:,4);
+
+figure;
+tile = tiledlayout(2,1);
+
+odo_ax = nexttile;
+plot(x,y);
+
+thet_ax = nexttile;
+plot(time,theta);
+
+figure;
+frame_gcf = gcf;
+tile =  tiledlayout(2,1);
+tile.TileSpacing = 'compact';
+tile.Padding = 'compact';
+
+odo_ax = nexttile;
+hold on;
+odo_line = plot(odo_ax, x(1),y(1), '--ob');
+
+theta_ax = nexttile;
+hold on;
+theta_line = plot(theta_ax, time(1), theta(1), '--ob');
+theta_ax.XLim = [min(time), max(time)];
+theta_ax.YLim = [min(theta), max(theta)];
+
+
+for i = 2:length(time)
+    tic
+    dt = (time(i) - time(i-1));
+
+    title(tile,['time: ',num2str(time(i))]);
+
+    plot(odo_ax,[x(i-1) x(i)],[y(i-1) y(i)],'-b');
+    odo_line.XData = x(i);
+    odo_line.YData = y(i);
+
+    plot(theta_ax,[time(i-1) time(i)],[theta(i-1) theta(i)],'-b');
+    theta_line.XData = time(i);
+    theta_line.YData = theta(i);
+
+    drawnow;
+    tmp_time = toc;
+    % pause(dt - tmp_time);
+    % drawnow limitrate
+end
